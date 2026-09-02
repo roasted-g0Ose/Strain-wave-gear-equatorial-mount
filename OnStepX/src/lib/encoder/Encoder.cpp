d020568fca5616291ac8f3d9d8146107969f9633
roasted-g0Ose <l.hyeokmin0618@gmail.com>
2026-09-02 23:24:31 +0900

    Add OnStepX firmware

---
+++ OnStepX/src/lib/encoder/Encoder.cpp
@@ -0,0 +1,13 @@
+// general purpose encoder class
+
+#include "Encoder.h"
+
+// get device ready for use
+void Encoder::init() {
+}
+
+// set encoder origin
+void Encoder::setOrigin(uint32_t count) {
+  origin = count;
+}
+
